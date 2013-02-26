685bb88a3ba56e0d44b2bbc664cfb8076c549513
Chris Wood <chriswood.ca@gmail.com>
2013-02-26 09:34:34 -0800

    Updated bootstrap to work with latest revision

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
+rm -rf LaurentGomila-SFML-*
 yum install -y cmake mesa-libGL-devel libXrandr-devel freetype-devel glew-devel libjpeg-devel openal-soft-devel libsndfile-devel fontconfig-devel
 wget https://github.com/LaurentGomila/SFML/tarball/master -O sfml.tgz
 tar -xvzf sfml.tgz
-cd LaurentGomila-SFML-4fbefe7
+cd LaurentGomila-SFML-*
 cmake .
 make install -j 4
