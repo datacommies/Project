d2fe13ef1681082e19dd39f2d8b7bafdb694f874
Steve Lo <kashun_lo@hotmail.com>
2013-02-27 09:54:57 -0800

    modified player.h

--- gameLogic/source/server/Player.h
+++ gameLogic/source/server/Player.h
@@ -3,7 +3,22 @@
 
 class Player {
 private:
-  long _id;
+	long _id;
+	int _hp;
+	string _class;
+	Point _currentCoordinates;
+	int _attackPoint;
+	int _defendPoint;
+	//perhaps some other status to the character
+	int _movementSpeed;
+
+	int _status;	//ready or not, which stage is he/she on...
+	//etc...
+  
+public:
+	long getID();
+	void setID(long);
+	
 };
 
 #endif
