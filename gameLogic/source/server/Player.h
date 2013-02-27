f8987667fb1628ef2f74ca3ccad988b1b19b236c
Darry <darry.d@gmail.com>
2013-02-26 16:06:25 -0800

    Renamed darry/ to server/

---
+++ gameLogic/source/server/Player.h
@@ -0,0 +1,9 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+class Player {
+private:
+  long _id;
+};
+
+#endif
---
+++ gameLogic/source/server/server.cpp
@@ -0,0 +1,11 @@
+#include "Team.h"
+
+#define NUM_OF_TEAMS 2
+
+Team teams[NUM_OF_TEAMS];
+
+
+int main() {
+
+
+}
