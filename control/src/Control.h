22fa8c63fac7de37afff1051a390ba53cb11a054
John <john@Evil-Sasha.(none)>
2013-02-28 10:20:01 -0800

    Added Control header as well as modules for Menu- and Playermodes. THe header is intended to be the include header for all Control files.

---
+++ control/src/Control.h
@@ -0,0 +1,29 @@
+/*--------------------------------------------------------------------------------------------------------------------
+-- PROGRAM: DTD-Control
+--
+-- FUNCTIONS: int AttackWithPlayer()
+--            int MovePlayer()
+--            int PlayerInputMonitor()
+--            int MenuInputMonitor()
+--
+-- DATE: 2013/02/25
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- NOTES: This is the control sub-program intended to be used
+--        in the Datacomm Tower Defence (DTD).
+-- 
+----------------------------------------------------------------------------------------------------------------------*/
+
+#define BUILDER_MODE 20
+#define PLAYER_MODE 30
+
+#define DIRECTION_UP 0
+#define DIRECTION_RIGHT 1
+#define DIRECTION_DOWN 2
+#define DIRECTION_LEFT 3
+
