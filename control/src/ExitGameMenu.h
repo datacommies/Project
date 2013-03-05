55bba8f331a7be307e861ce2e495bfca731116e5
Luke <coolman0893@gmail.com>
2013-03-04 23:01:43 -0800

    Added Exit Game Menu functions

---
+++ control/src/ExitGameMenu.h
@@ -0,0 +1,27 @@
+#ifndef EXITGAMEMENU_H
+#define EXITGAMEMENU_H
+
+/*--------------------------------------------------------------------------------------------------------------------
+-- PROGRAM: DTD-Control
+--
+-- FUNCTIONS: bool ExitGameMenu()
+--	      bool ExitGame()
+--
+-- DATE: 2013/03/04
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Luke Tao
+--
+-- PROGRAMMER: Luke Tao
+--
+-- NOTES: This is the control sub-program (exit game portion) intended to be used
+--        in the Datacomm Tower Defence (DTD).
+-- 
+----------------------------------------------------------------------------------------------------------------------*/
+
+bool ExitGameMenu();
+bool ExitGame();
+
+
+#endif
