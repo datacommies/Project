27dd39c3f9cc514ceddf0dae47e82d4a75557908
root <coolman0893@gmail.com>
2013-03-11 12:09:32 -0700

    Added ControlManager.h headers

--- redesigned/control/ControlManager.h
+++ redesigned/control/ControlManager.h
@@ -3,6 +3,28 @@
 
 #include "ControlPlaceHolder.h"
 #include "ControlState.h"
+#include <vector>
+
+class ControlManager {
+
+    public:
+            
+        ControlManager
+        
+        void AddNewState(ControlState state);
+        int CheckState();
+        void SetNewActiveState();
+        
+        
+    private:
+        
+        std::vector<ControlState> StateCollection;
+        ControlState 
+        
+            
+
+
+};
 
 #endif
 
