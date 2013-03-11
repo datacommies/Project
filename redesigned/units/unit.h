9fc6536313b39c0aa32c54d9d1795329e94ae668
Nick Raposo <nickraposo17@gmail.com>
2013-03-11 14:38:50 -0700

    Update unit.h

--- redesigned/units/unit.h
+++ redesigned/units/unit.h
@@ -9,9 +9,11 @@ public:
    int id;
    Point position;
    int health;
-   int attackDamage;
-   int attackRange;
-   int attackSpeed;
+   int attackDamage;//damage that the unit deals per hit
+   int attackRange; //distance that the unit can successfully attack
+   int attackSpeed; //the speed which the attack counter has to hit in order to initiate an attack
+   int perception;  //distance that the unit can aquire a target
+   int attackCount; //counter between unit attacks
 
    bool isAlive() { return health == 0; }
    void serializeUnit(const Unit& unit);
