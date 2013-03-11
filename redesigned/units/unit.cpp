20b1061210bf9bb8f77176108836a89478e61a56
Callum Styan <callum@machine.(none)>
2013-03-11 16:45:30 -0700

    ctor implemented for unit class

--- redesigned/units/unit.cpp
+++ redesigned/units/unit.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+Unit::Unit(const int& uid, const int& pos, const int& hp, const int& atkdmg,
+           const int& atkrng, const int& atkspd, const int& percep, const int& atkcnt):
+           id(uid), position(pos), health(hp), attackDamage(atkdmg), attackRange(atkrng),
+           attackSpeed(atkspd), perception(percep), attackCount(atkcnt)
+{
+    //any required validation
+
+}
+
 string Unit::serializeUnit(const Unit& unit){
 
 }
