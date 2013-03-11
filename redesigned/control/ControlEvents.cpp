13b799f20a70c7c7da1b74a14613553212f168b3
John <john@Evil-Sasha.(none)>
2013-03-11 13:08:04 -0700

    Added functions to wrap sending events

---
+++ redesigned/control/ControlEvents.cpp
@@ -0,0 +1,268 @@
+
+#include "ControlEvents.h"
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallExitGameEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallExitGameEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Exiting the Game.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallExitGameEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallEnterLobbyEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallEnterLobbyEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Entering the lobby form the startup menu.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallEnterLobbyEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallMoveLeftEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallMoveLeftEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Moving left when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallMoveLeftEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallMoveRightEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallMoveRightEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Moving right when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallMoveRightEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallMoveUpEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallMoveUpEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Moving Up when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallMoveUpEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallMoveDownEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallMoveDownEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for Moving Down when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallMoveDownEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallAttackLeftEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallAttackLeftEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for attacking Left when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallAttackLeftEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallAttackRightEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallAttackRightEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for attacking Right when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallAttackRightEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallAttackUpEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallAttackUpEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for attacking Up when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallAttackUpEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallAttackDownEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallAttackDownEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for attacking Down when in player mode.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallAttackDownEvent()
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallBuildTowerEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallBuildTowerEvent(int x, int y)
+--            int x - The X-coordinates for where the tower should be placed
+--            int y - The Y-coordinates for where the tower should be placed
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event building a tower and passes in the desired position.
+----------------------------------------------------------------------------------------------------------------------*/
+void CallBuildTowerEvent(int x, int y)
+{
+	
+}
+
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: CallBuildCreepEvent
+--
+-- DATE: 2013/03/11
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: void CallBuildCreepEvent()
+--
+-- RETURNS: void
+--
+-- NOTES: Calls the event for building a Creep
+----------------------------------------------------------------------------------------------------------------------*/
+void CallBuildCreepEvent()
+{
+	
+}
---
+++ redesigned/control/ControlEvents.h
@@ -0,0 +1,19 @@
+
+#include "resources.h"
+
+void CallExitGameEvent();
+
+void CallEnterLobbyEvent();
+
+void CallMoveLeftEvent();
+void CallMoveRightEvent();
+void CallMoveUpEvent();
+void CallMoveDownEvent();
+
+void CallAttackLeftEvent();
+void CallAttackRightEvent();
+void CallAttackUpEvent();
+void CallAttackDownEvent();
+
+void CallBuildTowerEvent(int x, int y);
+void CallBuildCreepEvent();
