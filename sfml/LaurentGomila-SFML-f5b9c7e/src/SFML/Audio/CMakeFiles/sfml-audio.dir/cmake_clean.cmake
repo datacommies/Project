50b6b7c59f92b758ea6d9a24fc2e21d8568a8a84
root <root@localhost.localdomain>
2013-03-11 14:39:52 -0700

    changed Makefile

---
+++ sfml/LaurentGomila-SFML-f5b9c7e/src/SFML/Audio/CMakeFiles/sfml-audio.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sfml-audio.dir/ALCheck.cpp.o"
+  "CMakeFiles/sfml-audio.dir/AudioDevice.cpp.o"
+  "CMakeFiles/sfml-audio.dir/Listener.cpp.o"
+  "CMakeFiles/sfml-audio.dir/Music.cpp.o"
+  "CMakeFiles/sfml-audio.dir/Sound.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundBuffer.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundBufferRecorder.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundFile.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundRecorder.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundSource.cpp.o"
+  "CMakeFiles/sfml-audio.dir/SoundStream.cpp.o"
+  "../../../lib/libsfml-audio.pdb"
+  "../../../lib/libsfml-audio.so"
+  "../../../lib/libsfml-audio.so.2.0"
+  "../../../lib/libsfml-audio.so.2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sfml-audio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ sfml/src/SFML/Window/CMakeFiles/sfml-window.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sfml-window.dir/Context.cpp.o"
+  "CMakeFiles/sfml-window.dir/GlContext.cpp.o"
+  "CMakeFiles/sfml-window.dir/GlResource.cpp.o"
+  "CMakeFiles/sfml-window.dir/Joystick.cpp.o"
+  "CMakeFiles/sfml-window.dir/JoystickManager.cpp.o"
+  "CMakeFiles/sfml-window.dir/Keyboard.cpp.o"
+  "CMakeFiles/sfml-window.dir/Mouse.cpp.o"
+  "CMakeFiles/sfml-window.dir/VideoMode.cpp.o"
+  "CMakeFiles/sfml-window.dir/Window.cpp.o"
+  "CMakeFiles/sfml-window.dir/WindowImpl.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/Display.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/GlxContext.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/InputImpl.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/JoystickImpl.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/VideoModeImpl.cpp.o"
+  "CMakeFiles/sfml-window.dir/Linux/WindowImplX11.cpp.o"
+  "../../../lib/libsfml-window.pdb"
+  "../../../lib/libsfml-window.so"
+  "../../../lib/libsfml-window.so.2.0"
+  "../../../lib/libsfml-window.so.2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sfml-window.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
