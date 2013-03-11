b54cd05016e1531afbedee9db709eea5a2512986
David Czech <davidczech@acm.org>
2013-03-11 01:09:10 -0700

    Added Makefile, split into two binaries:

---
+++ redesigned/client.cpp
@@ -0,0 +1,9 @@
+#include "resource.h"
+#include "modules/client.h"
+#include "modules/server.h"
+
+int main()
+{
+   Client();
+   return 0;
+}
---
+++ redesigned/server.cpp
@@ -0,0 +1,9 @@
+#include "resource.h"
+#include "modules/client.h"
+#include "modules/server.h"
+
+int main()
+{
+   Server();
+   return 0;
+}
