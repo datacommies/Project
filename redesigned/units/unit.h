e716dc73219557cc3b15997bc7ade60968e426ef
Nick Raposo <nickraposo17@gmail.com>
2013-03-11 20:31:34 -0700

    Update unit.h

--- redesigned/units/unit.h
+++ redesigned/units/unit.h
@@ -7,22 +7,22 @@
 class Unit
 {
 public:
-	int id; //unit id
-	Point position;
-	int health;
-	int attackDamage;//damage that the unit deals per hit
-	int attackRange; //distance that the unit can successfully attack
-	int attackSpeed; //the speed which the attack counter has to hit in order to initiate an attack
-	int perception;  //distance that the unit can aquire a target
-	int attackCount; //counter between unit attacks
-	bool isAlive() { return health == 0; }
-	std::string serializeUnit(const Unit& unit);
-	
-   Unit(const int& uid, Point pos, const int& hp, const int& atkdmg, const int& atkrng,
+    int id; //unit id
+    Point position;
+    int health;
+    int attackDamage;//damage that the unit deals per hit
+    int attackRange; //distance that the unit can successfully attack
+    int attackSpeed; //the speed which the attack counter has to hit in order to initiate an attack
+    int perception;  //distance that the unit can aquire a target
+    int attackCount; //counter between unit attacks
+    bool isAlive() { return health <= 0; } //incase creep get hits more than its remaining health
+    std::string serializeUnit(const Unit& unit);
+    
+    Unit(const int& uid, Point pos, const int& hp, const int& atkdmg, const int& atkrng,
         const int& atkspd, const int& percep, const int& atkcnt);
-
-	virtual UnitTypes getType() const = 0;
-	virtual size_t getSize() const = 0;
+    
+    virtual UnitTypes getType() const = 0;
+    virtual size_t getSize() const = 0;
 private:   
 };
 
