0a33410625cadc2edcaa7fe1affe02e4fcec5e75
Codysr <codysrossiter@gmail.com>
2013-03-12 15:03:42 -0700

    Adding initial splash tower files

---
+++ redesigned/units/splash_tower.cpp
@@ -0,0 +1,13 @@
+#include "splash_tower.h"
+#include "unit.h"
+#include <string>
+
+using namespace std;
+
+SplashTower::SplashTower(const int& uid, Point pos, const int& hp, const int& atkdmg, const int& atkrng,
+            const int& atkspd, const int& percep, const int& atkcnt, const int& wall):
+            Tower(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, wall)
+{
+    //validation
+}
+
