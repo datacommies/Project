4560da262f04e7aa6783d98a50f067e8067454d5
John <john@Evil-Sasha.(none)>
2013-03-12 10:45:17 -0700

    Added Control functionailty to module/control

--- redesigned/modules/control.cpp
+++ redesigned/modules/control.cpp
@@ -1,4 +1,5 @@
 #include "control.h"
+#include "../control/Control.h"
 
 /* Constructor
  *
@@ -7,40 +8,9 @@
  * RETURNS: 
  * NOTES:   Creates a thread and starts running the module */
 Control::Control(ClientGameLogic& clientGameLogicModule)
-   : clientGameLogicModule_(clientGameLogicModule)
+	:clientGameLogicModule_(clientGameLogicModule)
 {
-   // TODO: create a thread and begin processing
-}
-
-/* Handles a UI request to create a unit.
- *
- * PRE:     Create Unit UI element is available to user.
- * POST:    Request for game logic to create unit has been sent.
- * RETURNS: 
- * NOTES:   Performs no validation. Validation is handled by game logic module. */
-void Control::createUnit(UnitType type, Point location)
-{
-   clientGameLogicModule_.createUnit(type, location);
-}
-
-/* Handles a UI request to move a player.
- *
- * PRE:     move player UI element is available to user.
- * POST:    Request for game logic to move player has been sent.
- * RETURNS: 
- * NOTES:   Performs no validation. Validation is handled by game logic module. */
-void Control::movePlayer(Direction direction)
-{
-   clientGameLogicModule_.movePlayer(direction);
-}
-
-/* Handles a UI request to attack.
- *
- * PRE:     Attack UI element is available to user.
- * POST:    Request for game logic to attack has been sent.
- * RETURNS: 
- * NOTES:   Performs no validation. Validation is handled by game logic module. */
-void Control::attack(Direction direction)
-{
-   clientGameLogicModule_.attack(direction);
+	// TODO: create a thread
+	loadControlEvents(clientGameLogicModule);
+	ControlRun();
 }
