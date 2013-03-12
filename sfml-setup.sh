4f15e6322c171b8f3e26236009b8d691770b3595
root <root@machine.(none)>
2013-03-12 13:36:54 -0700

    updated sfml script to install font config for debian systems

--- sfml-setup.sh
+++ sfml-setup.sh
@@ -9,7 +9,7 @@ read -p "Choice [0,1]: " distro
 case $distro in
     0) yum install -y wget cmake mesa-libGL-devel libXrandr-devel freetype-devel glew-devel libjpeg-devel openal-soft-devel libsndfile-devel fontconfig-devel gcc-c++
         ;;
-    1) apt-get install -y cmake libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libglew1.5-dev libjpeg8-dev libsndfile1-dev libopenal-dev
+    1) apt-get install -y cmake libpthread-stubs0-dev libgl1-mesa-dev libx11-dev libxrandr-dev libfreetype6-dev libglew1.5-dev libjpeg8-dev libsndfile1-dev libopenal-dev libfontconfig1-dev
         ;;
     *) echo Invalid Input;
         exit 1
