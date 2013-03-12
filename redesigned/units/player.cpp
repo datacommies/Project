3ccb9d32e7eba86bae5c72b23bbdf82734d512b0
Callum Styan <callum@machine.(none)>
2013-03-11 17:16:30 -0700

    updated ctors for player

--- redesigned/units/player.cpp
+++ redesigned/units/player.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 
+Player::Player(const int& uid, const int& pos, const int& hp, const int& atkdmg, const int& atkrng,
+               const int& atkspd, const int& percep, const int& atkcnt, const int& spd, Direction direct,
+               const int& cid): MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, )
+
 string Player::serializePlayer(const Player& player)
 {
 	
