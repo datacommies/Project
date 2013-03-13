15cdaa60398702cd47b3be87cf6e5cd1e8dc3952
aikorose <aikorose7@gmail.com>
2013-03-13 14:40:46 -0700

    Create electro_tower.h

---
+++ redesigned/units/electro_tower.h
@@ -0,0 +1,17 @@
+#ifndef ELECTRO_TOWER_H
+#define ELECTRO_TOWER_H
+
+#include "tower.h"
+
+class ElectroTower : public Tower
+{
+public:
+
+    ElectroTower(int uid, Point pos, int hp, int atkdmg, int atkrng,
+int atkspd, int percep, int atkcnt, int wall);
+
+
+private:
+};
+
+#endif
