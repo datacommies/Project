feff479ae8e2a6cd7fd9b66b8891e686caf6ab5c
aikorose <aikorose7@gmail.com>
2013-03-13 03:28:09 -0700

    Update Point.h

--- redesigned/units/Point.h
+++ redesigned/units/Point.h
@@ -2,9 +2,35 @@
 #define POINT_H
 
 class Point {
+  private:
+		double x;
+		double y;
   public:
-		int x;
-		int y;
+  		Point(double xp = 0, double yp = 0){
+  			x = xp; 
+  			y = yp;
+  		}
+  		
+  		double getX()const;
+  		
+  		double getY()const;
+  		
+  		void setX(const double& xp);
+  		
+  		void setY(const double& yp);
+  		
+  		double distance(Point otherP){
+  			double xdis = x - otherP.x;
+  			double ydis = y - otherP.y;
+  			return sqrt(xdis*xdis + ydis*ydis);
+  		}
+  		
+  		void move(double a, double b){
+  			x += a;
+  			y += b;
+  		}
+  		
+  		
 };
 
 #endif
