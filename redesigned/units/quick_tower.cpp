e61f794bfd7aaa47993152dc70ecebd6d6aff711
Codysr <codysrossiter@gmail.com>
2013-03-13 12:25:23 -0700

    Added basic implementation for quick and basic towers

--- redesigned/units/quick_tower.cpp
+++ redesigned/units/quick_tower.cpp
@@ -1,14 +1,14 @@
 /*
- * Splash Damage tower attacks single unit and damage spreads to nearby units
+ * Quick tower targets one unit and fires rapidly at it; Low damage tower.
  * - Basic implementation
 */
-#include "splash_tower.h"
+#include "quick_tower.h"
 #include "unit.h"
 #include <string>
 
 using namespace std;
 
-SplashTower::SplashTower(const int& uid, Point pos, const int& hp, const int& atkdmg, const int& atkrng,
+QuickTower::QuickTower(const int& uid, Point pos, const int& hp, const int& atkdmg, const int& atkrng,
             const int& atkspd, const int& percep, const int& atkcnt, const int& wall):
             Tower(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, wall)
 {
