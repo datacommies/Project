20abc7517e10a00d6d73c1280214553bbbdf2500
Codysr <codysrossiter@gmail.com>
2013-03-14 12:53:45 -0700

    add basic attack function to splash tower; Modified basic towers attack function & added comments

--- redesigned/units/splash_tower.cpp
+++ redesigned/units/splash_tower.cpp
@@ -15,3 +15,25 @@ SplashTower::SplashTower(	int uid, Point pos, int hp, int atkdmg, int atkrng,
     //validation
 }
 
+void SplashTower::Attack(void)
+{
+    // check if we can attack
+    if( attackCount++ < attackSpeed )
+        return;
+        
+    attackCount = 0;
+    pTarget->health -= attackDamage;
+    /* check for nearby enemies & damage them
+       for ( enemy units )
+       {
+           if ( in range )
+           {
+               damage unit
+           }
+       }
+    */
+
+    // check if target is dead or in range (might have to call this at start of
+    // function instead)
+    CheckTarget();
+}
