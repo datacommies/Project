8ed41365dff94dd0007a87c58bdbd403bddac159
Codysr <codysrossiter@gmail.com>
2013-03-14 14:30:14 -0700

    Added code and variables for splash damage in splash_tower.cpp; added splash_tower.cpp to makefile

--- redesigned/units/splash_tower.h
+++ redesigned/units/splash_tower.h
@@ -3,12 +3,17 @@
 
 #include "tower.h"
 
+
 class SplashTower : public Tower
 {
 public:
-
+    Team * team;            // other team for splash damage calculations
+    int    splashDamage;
+    int    splashRange;  // Range in which we can damage other units   
+    
     SplashTower(int uid, Point pos, int hp, int atkdmg, int atkrng,
-				int atkspd, int percep, int atkcnt, int wall);
+				int atkspd, int percep, int atkcnt, int wall, Team * t,
+				int spldmg, int splrng);
     
     virtual void Attack(void);
 	
