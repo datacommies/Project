6cc47c178d4f411dc39bce3b49e74e9d490998da
John <john@Evil-Sasha.(none)>
2013-03-15 11:09:37 -0700

    Added function header for constructor

--- redesigned/control/BuildController.cpp
+++ redesigned/control/BuildController.cpp
@@ -2,6 +2,27 @@
 #include "ControlEvents.h"
 #include "../modules/graphics.h"
 
+/*-------------------------------------------------------------------------------------------------------------------- 
+-- FUNCTION: Controller
+--
+-- DATE: 2013/03/15
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- INTERFACE: BuildController(int returnValue, sf::Keyboard::Key key, Button buttonRect, void (*move_fp)(void))
+--            int returnValue - The value that theCheckController() function returns on success
+--            Key key - The Key that is being listen to for input
+--            Button buttonRect - The Button that is being listen to for input
+--            void (*move_fp)(void) - A function pointer to the function that should be called by RunCommand
+--
+-- RETURNS: N/A
+--
+-- NOTES: Constructor for BuildController Class
+----------------------------------------------------------------------------------------------------------------------*/
 BuildController::BuildController(int returnValue, sf::Keyboard::Key key, Button buttonRect, void (*move_fp)(void *))
 	:Controller(returnValue, key, buttonRect, move_fp)
 {
