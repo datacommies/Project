a9392bb16939a614e3794bdd832b58ba83b1983d
Jesse Wright <jesse.wright16@gmail.com>
2013-03-20 11:13:43 -0700

    Added the graphics object to be passed into the Control constructor. Currently neededing to create a thread but some problems have occured so it is commented out at the moment.

--- redesigned/modules/control.cpp
+++ redesigned/modules/control.cpp
@@ -1,15 +1,33 @@
 #include "control.h"
-
+#include "pthread.h"
+#include <X11/Xlib.h>
+#include <SFML/System.hpp>
 /* Constructor
  *
  * PRE:     
  * POST:    
  * RETURNS: 
  * NOTES:   Creates a thread and starts running the module */
-Control::Control(ClientGameLogic& clientGameLogicModule)
-	:clientGameLogicModule_(clientGameLogicModule)
+Control::Control(ClientGameLogic& clientGameLogicModule, Graphics& graphics)
+	:clientGameLogicModule_(clientGameLogicModule), graphicsModule_(graphics)
 {
 	// TODO: create a thread
 	//loadControlEvents(&clientGameLogicModule);
 	//ControlRun();
+	//XInitThreads();
+	//pthread_t thread;
+	//int result;
+	
+	//loadControlEvents(&clientGameLogicModule, &graphicsModule_);
+	//sf::Thread Thread(&ControlRun);
+	//Thread.launch();
+	
+	//result = pthread_create(&thread, NULL, ControlRun, NULL);
+	//if(result)
+	//{
+	    
+	    // error
+	//}
+	
+	
 }
