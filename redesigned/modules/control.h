744ce6d2c6a7f52b93c17280f84f9eebc2e48ca3
John <john@Evil-Sasha.(none)>
2013-03-21 18:52:38 -0700

    Redesigned Control Completely to run out of this class (And not require threading)

--- redesigned/modules/control.h
+++ redesigned/modules/control.h
@@ -1,21 +1,81 @@
 #ifndef CONTROL_MODULE_H
 #define CONTROL_MODULE_H
+/*--------------------------------------------------------------------------------------------------------------------
+-- CLASS: Control
+--
+-- FUNCTIONS: get()
+--            Control()
+--            void LoadGameLogic()
+--            void LoadGraphics()
+--            void AddNewCalledButton()
+--            void AddNewCalledKey()
+--            void RunAllButtons()
+--            void RunAllKeys()
+--
+-- DATE: 2013/03/21
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: John Payment
+--
+-- PROGRAMMER: John Payment
+--
+-- NOTES: This class is the central Control Module Singleton. It is called to generate lists of Control Events, check
+--        each of those events against lists of valid events, and if a match is found run Wrapper functions. It also contains
+--        those wrapper function.
+--
+--        Control Event Definition - Any time a Key or Button is pressed is a Control Event
+-- 
+----------------------------------------------------------------------------------------------------------------------*/
 
 #include "../resource.h"
-#include "../control/Control.h"
 #include "client_game_logic.h"
+#include <vector>
 
 class ClientGameLogic;
 
 class Control
 {
 public:
-   // Constructors
-   Control(ClientGameLogic& clientGameLogicModule, Graphics& graphics);
+	// Get Method for Singleton Class
+	static void Control* get();
+	
+	// Initialization for Game Logic
+	void LoadGameLogic(ClientGameLogic& clientGameLogicModule);
+	void LoadGraphics(Graphics& graphicsModule);
+	
+	// Call Functions for reporting Control Events
+	void AddNewCalledButton(int id);
+	void AddNewCalledKey(sf::Keyboard::Key key);
+	
+	// Functions for Running Control Events
+	void RunAllButtons();
+	void RunAllKeys();
 private:
-   // Module references
-   ClientGameLogic& clientGameLogicModule_;
-   Graphics& graphicsModule_;
+	// Static Reference for Singleton Class
+	static Control* _Control;
+	
+	// Constructors
+	Control();
+	
+	// Collections for storing Control Events
+	std::vector<int> _buttonIDs;
+	std::vector<sf::Keyboard::Key> _keys;
+	
+	// Module references
+	ClientGameLogic& _clientGameLogicModule;
+	Graphics& _graphicsModule;
+	
+	// Wrapper Functions
+	void CallExitGameEvent();
+	void CallEnterLobbyEvent();
+
+	void CallMoveEvent(int direction);
+
+	void CallAttackEvent(int direction);
+
+	void CallBuildTowerEvent();
+	void CallBuildCreepEvent();
 };
 
 #endif
