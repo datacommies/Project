238a8aac3109a5baa82309dd73066968c3f94568
unclekevin <kevintangeman@dccnet.com>
2013-03-26 15:55:38 -0700

    Fixed the order of passing client ID "cid" into constructors.

--- redesigned/units/castle.cpp
+++ redesigned/units/castle.cpp
@@ -12,7 +12,7 @@ Castle::Castle(int uid, Point pos, int hp, int atkdmg, int atkrng,
     // validation
 }
 
-Castle::Castle(int cid, int uid, int side, Point pos, int hp, int atkdmg, int atkrng,
+Castle::Castle(int uid, int cid, int side, Point pos, int hp, int atkdmg, int atkrng,
     int atkspd, int percep, int atkcnt, int wall):
     Tower(uid, side, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, wall), 
     clientID_(cid)
--- redesigned/units/player.cpp
+++ redesigned/units/player.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 Player::Player(int uid, Point pos, int hp, int atkdmg, int atkrng,
                int atkspd, int percep, int atkcnt, int spd, Direction direct,
-               int cid) : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(clientID)
+               int cid) : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(cid)
 {
     //validation
 }
 
 Player::Player(int uid, int cid, Point pos, int hp, int atkdmg, int atkrng,
                int atkspd, int percep, int atkcnt, int spd, Direction direct)
-               : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(clientID)
+               : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(cid)
 {
     //validation
 }
