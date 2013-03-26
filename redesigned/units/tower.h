9722657da58ce4337e84bbfead09df1c825abef1
Steve <steve@localhost.localdomain>
2013-03-26 13:34:27 -0700

    tower.h default

--- redesigned/units/tower.h
+++ redesigned/units/tower.h
@@ -19,8 +19,8 @@ class Tower : public Unit
 private:
 public:
     int wallArmour_;
-    Tower(int uid, Point pos, int hp, int atkdmg, int atkrng,
-          int atkspd, int percep, int atkcnt, int wall);
+    Tower(int uid, Point pos, int hp=100, int atkdmg=0, int atkrng=0,
+          int atkspd=0, int percep=0, int atkcnt=0, int wall=0);
 
     std::string serializeTower();
 
