bc03ef8c6083e507d03d4ded96790d072cb9a70b
bahbas <behnam@live.ca>
2013-03-26 21:16:34 -0700

    added a comment to add code to send resources

--- redesigned/modules/server_network.cpp
+++ redesigned/modules/server_network.cpp
@@ -93,6 +93,8 @@ bool ServerNetwork::sync(int client_)
         send(client_, sc.data(), sc.size(), 0);
     }
 
+    //also send updated currency to teams[0] here
+
     /*for (size_t i = 0; i < serverGameLogic_.teams[0].players.size(); ++i)
     {
         string sc = serverGameLogic_.teams[0].players[i].serializePlayer();
@@ -118,6 +120,9 @@ bool ServerNetwork::sync(int client_)
         string sc = serverGameLogic_.teams[1].players[i].serializePlayer();
         send(client_, sc.data(), sc.size(), 0);
     }*/
+
+    //also send updated currency to teams[1] here
+
     return true;
 }
 
@@ -175,44 +180,6 @@ void* ServerNetwork::handleInput(void* args)
 void* ServerNetwork::handleClientRequest(void* args)
 {
     
-
-
-/*typedef struct $
-{
-    header_t head;
-    UnitType unit;
-    int posx, posy;
-} request_create_t;
-
-typedef struct {
-    int type; //MSG_PLAYER_UPDATE for e.g.
-    int size; //sizeof(player_matchmakcing_t) for eg.
-} header_t;
-
-enum UnitType
-{
-   PLAYER,
-   CREEP,
-   TOWER,
-   CASTLE
-};
-
-enum {
-    MSG_MAPNAME,            // Update everyone's mapname
-    MSG_START,              // It's time to start the game!
-    MSG_PLAYER_UPDATE, // Update a client's players container.
-    MSG_PLAYER_JOIN,
-    MSG_PLAYER_LEAVE,
-    MSG_CHAT,                // A chat message has been generated
-    MSG_CREATE_UNIT,
-    MSG_UPDATE_UNIT,
-    MSG_RESOURCE_UPDATE,
-    MSG_REQUEST_CREATE,
-    MSG_REQUEST_PLAYER_MOVE,
-    MSG_CLEAR
-};
-
-*/
     ClientCtx* ctx = (ClientCtx*)args;
     ServerNetwork* thiz = (ServerNetwork*) ctx->sn;
     int client_ = ctx->client;
@@ -275,7 +242,6 @@ enum {
     return NULL;
 }
 
-
 void* ServerNetwork::handleClient(void* args)
 {
     cout << "Handling client!" << endl;
@@ -294,79 +260,7 @@ void* ServerNetwork::handleClient(void* args)
         thiz->sync(client_);
         usleep(100000);
     }
-
-    /*
-    // Add this player first
-    player_matchmaking_t player;
-    
-    // if client_ chooses to be a player, add them to player list
-    if (recv_complete(client_, &player, sizeof(player), 0) > 0) {
-        player.pid = client_; // cheap and easy way of assigning a unique player id.
-        player.name[PLAYER_NAME_SIZE-1] = 0;
-        cout << "Player: " << player.name << " Team: " << player.team << endl;
-        thiz->players_.push_back(player);
-    }
-    
-    // Send all current players_. probably need mutex here.
-    for (size_t i = 0; i < thiz->players_.size(); i++) {
-        thiz->players_[i].head.type = MSG_PLAYER_UPDATE;
-        
-        send(client_, &thiz->players_[i], sizeof(player_matchmaking_t), 0);
-    }
-    //update_all_clients_(MSG_PLAYER_UPDATE);
-    //const char * data = sc.data();
-    header_t * head = (header_t*)&data;
-    cout << head->type << endl;*/
-
-    /*
-    player.head.type = MSG_PLAYER_UPDATE;
-    // Inform all other clients_ that this player has arrived.
-    for (size_t i = 0; i < thiz->clients_.size(); ++i)
-    {
-        if (thiz->clients_[i] != client_)
-            send(thiz->clients_[i], &player, sizeof(player_matchmaking_t), 0);
-    }
-    std::cout << "Sent current players_" << std::endl;
-    
-    // send map to new connected players_
-    map_t m = {{0, 0}, {0}};
-    m.head.type = MSG_MAPNAME;
-    strcpy(m.value, "FirstMap");
-    
-    send(client_, &m, sizeof(map_t), 0);
-    cout << "Send map name" << endl;
-    
-    // read commands
-    while (1) {
-        header_t head;
-        int n = recv_complete(client_, &head, sizeof(head), 0);
-        
-        if (n <= 0)
-            break;
-        
-        if (head.type == MSG_CHAT) {
-            char * buf = new char [head.size];
-            memset(buf, 0, head.size);
-            recv_complete(client_, buf, head.size, 0);
-            cout << "Got message:" << buf << "from client_" << endl;
-            delete buf;
-        }
-    }
-    
-    close(client_);
-    thiz->players_.erase(std::remove(thiz->players_.begin(), thiz->players_.end(), player), thiz->players_.end());
-    thiz->clients_.erase(std::remove(thiz->clients_.begin(), thiz->clients_.end(), client_), thiz->clients_.end());
-    
-    player.head.type = MSG_PLAYER_LEAVE;
-    for (size_t i = 0; i < thiz->clients_.size(); ++i)
-    {
-        send(thiz->clients_[i], &player, sizeof(player_matchmaking_t), 0);
-    }
-    
-    cout << endl << "client_ disconnected." << endl << "server> ";
-    cout.flush();
-    */
-    
+ 
     return NULL;
 }
 
@@ -396,7 +290,7 @@ int recv_complete (int sockfd, void *buf, size_t len, int flags) {
     return bytesRead;
 }
 
-// Move me!
+// Move me! //NO!
 bool operator == (const player_matchmaking_t& a, const player_matchmaking_t& b) {
     return a.pid == b.pid;
 }
