abdcab7be293704758f7e443db56126dcc4b0ed3
Albert Liao <liao.a17@gmail.com>
2013-04-09 15:45:03 -0700

    fixed build. players wasnt updated and server_game_logic.h

--- redesigned/units/player.cpp
+++ redesigned/units/player.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-Player::Player(int uid, int cid, Point pos, int hp, int atkdmg, int atkrng,
+Player::Player(int uid, int cid, Point pos, int role, int hp, int atkdmg, int atkrng,
                int atkspd, int percep, int atkcnt, int spd, Direction direct)
-               : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(cid)
+               : MobileUnit(uid, pos, hp, atkdmg, atkrng, atkspd, percep, atkcnt, spd, direct), clientID(cid), role(role)
 {
     //validation
 }
