e1669c4d758bcd26fb9108856f4699f2827cf939
Darry <darry.d@gmail.com>
2013-04-09 17:03:02 -0700

    more updating of maps

--- redesigned/modules/server_game_logic.cpp
+++ redesigned/modules/server_game_logic.cpp
@@ -406,6 +406,7 @@ void ServerGameLogic::updateCreate(CommandData& command)
     return;
   }
 
+  updateMaps();
   if (!mapTeams_[0].isValidPos(command.location)) {
   //  fprintf(stderr, "max x: %d max y: %d\n", MAX_X, MAX_Y);
  //   fprintf(stderr, "x: %d, y: %d out of range: %s line %d\n", x, y, __FILE__, __LINE__);
