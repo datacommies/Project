a111cbc6791aebfdb0796aea8715370546670c48
Aaron Lee <aaron@onefatgiraffe.com>
2013-04-10 17:46:58 -0700

    comments audio module

--- redesigned/modules/audio.cpp
+++ redesigned/modules/audio.cpp
@@ -3,12 +3,14 @@
 --
 -- DATE:        2013/03/11
 --
--- MAINTAINERS: 
+-- MAINTAINERS: Aaron Lee, Luke Tao
 --
--- FUNCTIONS:   Client constructor
---              run
+-- FUNCTIONS:   Audio constructor
+--              playMusic
+--              stopMusic
+--              pauseMusic
 --
--- DESCRIPTION: File contains implementation for the Client class.
+-- DESCRIPTION: File contains implementation for the Audio class.
 ------------------------------------------------------------------------------*/
 #include "audio.h"
 
@@ -18,18 +20,18 @@ Audio::Audio()
 }
 
 /*------------------------------------------------------------------------------
--- FUNCTION:    run
+-- FUNCTION:    playMusic
 --
 -- DATE:        2013/03/11
 --
--- DESIGNER:    
--- PROGRAMMER:  
+-- DESIGNER:    Luke Tao
+-- PROGRAMMER:  Aaron Lee
 --
--- INTERFACE:   void Client::run()
+-- INTERFACE:   void Audio::playMusic()
 --
--- RETURNS:     void
+-- RETURNS:     int -- 1 if failed to open file
 --
--- DESCRIPTION: This function will continually attempt to connect to the server.
+-- DESCRIPTION: load music file then play it
 ------------------------------------------------------------------------------*/
 int Audio::playMusic(const char* filename)
 {
@@ -45,11 +47,41 @@ int Audio::playMusic(const char* filename)
     return EXIT_SUCCESS;
 }
 
+
+/*------------------------------------------------------------------------------
+-- FUNCTION:    stopMusic
+--
+-- DATE:        2013/03/11
+--
+-- DESIGNER:    Luke Tao
+-- PROGRAMMER:  Aaron Lee
+--
+-- INTERFACE:   void Audio::stopMusic()
+--
+-- RETURNS:     void
+--
+-- DESCRIPTION: stop currently playing music file
+------------------------------------------------------------------------------*/
 void Audio::stopMusic()
 {
     music_.stop();
 }
 
+
+/*------------------------------------------------------------------------------
+-- FUNCTION:    pauseMusic
+--
+-- DATE:        2013/03/11
+--
+-- DESIGNER:    Luke Tao
+-- PROGRAMMER:  Aaron Lee
+--
+-- INTERFACE:   void Audio::pauseMusic()
+--
+-- RETURNS:     void
+--
+-- DESCRIPTION: pause currently playing music file
+------------------------------------------------------------------------------*/
 void Audio::pauseMusic()
 {
     music_.pause();
